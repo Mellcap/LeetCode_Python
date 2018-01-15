f478a240318f3888467842fed2408f132fddd06f
Mellcap <imellcap@gmail.com>
2018-01-15 14:17:59 +0800

    4.MedianOfTwoSortedArrays. -- Improved Solution & Instruction

--- 4_MedianOfTwoSortedArrays.py
+++ 4_MedianOfTwoSortedArrays.py
@@ -77,8 +77,80 @@ class Solution:
 
 # Improved version
                 
+class Solution:
+    
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        The core is to find the (K+1)th num.
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        totalLength = len(nums1) + len(nums2)
+        if totalLength % 2 == 1:
+            k = totalLength // 2
+            return self.findKth(nums1, nums2, k)
+        else:
+            k1 = totalLength // 2
+            k2 = k1 - 1
+            return (self.findKth(nums1, nums2, k1) + self.findKth(nums1, nums2, k2)) / 2
+        
+    def findKth(self, nums1, nums2, k):
+        m, n = len(nums1), len(nums2)
+
+        if m > n:  # make sure the first array smaller
+            return self.findKth(nums2, nums1, k)
+        
+        left, right = -1, m                                        # [-1, m) => include the left one, exclude the right one.
+        while left < right - 1:                                    # right - 1 == m - 1, which is the last index
+            mid = left + (right - left) // 2
+            kNext = k - mid
+            if 0 <= kNext < n and nums1[mid] > nums2[kNext]:       # mid not meet the condition, so exclude mid
+                right = mid
+            else:                                                  # mid meet the condition, so include mid
+                left = mid
+            
+        k1 = nums1[left] if left >= 0 else float('-inf')
+        k2 = nums2[k - left - 1] if (k - left - 1) >= 0 else float('-inf')
+        
+        return max(k1, k2)
+
 
 # ----------------------------------
 # Instruction
 # ----------------------------------
+
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        len1, len2 = len(nums1), len(nums2)
+        if (len1 + len2) % 2 == 1: 
+            return self.getKth(nums1, nums2, (len1 + len2)/2 + 1)
+        else:
+            return (self.getKth(nums1, nums2, (len1 + len2)/2) + \
+                    self.getKth(nums1, nums2, (len1 + len2)/2 + 1)) * 0.5
+
+    def getKth(self, A, B, k):
+        m, n = len(A), len(B)
+        if m > n:
+            return self.getKth(B, A, k)
+
+        left, right = 0, m    
+        while left < right:
+            mid = left + (right - left) / 2
+            if 0 <= k - 1 - mid < n and A[mid] >= B[k - 1 - mid]:
+                right = mid
+            else:
+                left = mid + 1
+
+        Ai_minus_1 = A[left - 1] if left - 1 >= 0 else float("-inf")
+        Bj = B[k - 1 - left] if k - 1 - left >= 0 else float("-inf")
+
+        return max(Ai_minus_1, Bj)
+
+
 
